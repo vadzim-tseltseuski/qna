bc121d1dc181ad667cb4089ddac1c2ff8a75d3d6
vadimteltevski <vadzim.tseltseuski@softswiss.com>
2022-07-26 00:38:06 +0400

    feat: add answer model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_25_201718) do
+ActiveRecord::Schema.define(version: 2022_07_25_203419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.text "body", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "title"
