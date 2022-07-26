1b1bae081f603c9eeff649b258d083d4be1669c0
vadimteltevski <vadzim.tseltseuski@softswiss.com>
2022-07-26 22:01:40 +0400

    fix

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Question < ApplicationRecord
-  has_many :answer, dependent: :destroy
+  has_many :answers, dependent: :destroy
 
   validates :title, :body, presence: true
 end
